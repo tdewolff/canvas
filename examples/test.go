5d935c29928bf6a7ae7fe46253edf89d5eb1f675
Taco de Wolff <tacodewolff@gmail.com>
2019-10-11 09:50:32 -0300

    Update test

--- examples/test.go
+++ examples/test.go
@@ -58,13 +58,13 @@ func draw(c *canvas.Canvas) {
 	//c.DrawPath(10.0, 10.0, p)
 	//c.DrawPath(10.0, 10.0, p.Transform(canvas.Identity.Shear(2.0, -1.0)))
 	//s := "ﻙﺎﻨﺗ ﺎﻠﺴﻣﺍﺀ ﺹ \n\n ﺎﻔﻳﺓ ﻢﻧ"
-	s := "Lorem ipsum dolor sid amet"
+	//s := "Lorem ipsum dolor sid amet"
 
-	face := font.Face(6.0, canvas.Black, canvas.FontRegular, canvas.FontNormal)
-	//frame := canvas.Rectangle(28.0, -10.0)
-	rt := canvas.NewRichText()
-	rt.Add(face, s)
-	text := rt.ToText(28.0, 0.0, canvas.Justify, canvas.Top, 0.0, 0.0)
+	//face := font.Face(6.0, canvas.Black, canvas.FontRegular, canvas.FontNormal)
+	////frame := canvas.Rectangle(28.0, -10.0)
+	//rt := canvas.NewRichText()
+	//rt.Add(face, s)
+	//text := rt.ToText(28.0, 0.0, canvas.Justify, canvas.Top, 0.0, 0.0)
 
 	//p, _ := canvas.ParseSVG(fmt.Sprintf("A20.0 10.0 0.0 0 0 40.0 0.0z"))
 	//f := p.Flatten().Stroke(1.0, canvas.ButtCapper, canvas.MiterJoiner)
@@ -74,9 +74,9 @@ func draw(c *canvas.Canvas) {
 	//c.DrawPath(10.0, 10.0, frame)
 	//c.DrawText(10.0, 10.0, text)
 
-	ps, _ := text.ToPaths()
-	fmt.Println(len(ps))
-	c.DrawPath(10.0, 10.0, ps[0].Stroke(1.0, canvas.ButtCapper, canvas.MiterJoiner))
+	//ps, _ := text.ToPaths()
+	//fmt.Println(len(ps))
+	//c.DrawPath(10.0, 10.0, ps[0].Stroke(1.0, canvas.ButtCapper, canvas.MiterJoiner))
 
 	//c.SetView(canvas.Identity.Rotate(45).Scale(2.0, 1.0))
 
@@ -110,17 +110,17 @@ func draw(c *canvas.Canvas) {
 	//p = p.Offset(1.0)
 	//c.DrawPath(110, 110, p)
 
-	//ellipse, _ := canvas.ParseSVG(fmt.Sprintf("A20 40 0 0 0 40 0z"))
-	//c.SetFillColor(canvas.Red)
-	//c.DrawPath(10.0, 10.0, ellipse)
+	ellipse, _ := canvas.ParseSVG(fmt.Sprintf("A100 200 30 1 0 200 0z"))
+	c.SetFillColor(canvas.Red)
+	c.DrawPath(10.0, 10.0, ellipse)
 
-	//ellipse = ellipse.Flatten()
-	//ps := ellipse.SplitAt(10.0)
-	//ellipse = ps[0]
+	ps := ellipse.SplitAt(100.0)
+	ellipse = ps[0]
 	//fmt.Println(ellipse)
-	//ellipse = ellipse.Stroke(2.0, canvas.RoundCapper, canvas.RoundJoiner)
-	//c.SetFillColor(color.RGBA{0, 0, 0, 128})
-	//c.DrawPath(10.0, 10.0, ellipse)
+	//ellipse = ellipse.Dash(0.0, 20.0, 40.0, 20.0)
+	ellipse = ellipse.Stroke(5.0, canvas.ButtCapper, canvas.RoundJoiner)
+	c.SetFillColor(color.RGBA{0, 0, 0, 128})
+	c.DrawPath(10.0, 10.0, ellipse)
 
 	//drawStrokedPath(c, 30, 40, 2.0, "M0 0L50 0L50 -5")
 	//drawStrokedPath(c, 30, 30, 2.0, "M-25 -25A25 25 0 0 1 0 0A25 25 0 0 1 25 -25z")
