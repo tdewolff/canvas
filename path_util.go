340bdd6d509ace0da8c2006479fb3fa543c11c68
Taco de Wolff <tacodewolff@gmail.com>
2019-10-29 13:46:31 -0300

    Add intersections file

--- path_util.go
+++ path_util.go
@@ -4,66 +4,6 @@ import (
 	"math"
 )
 
-// intersection between two line segments
-// see http://www.cs.swan.ac.uk/~cssimon/line_intersection.html
-func intersectionLineLine(a0, a1, b0, b1 Point) (Point, bool) {
-	da := a1.Sub(a0)
-	db := b1.Sub(b0)
-	div := da.PerpDot(db)
-	if equal(div, 0.0) {
-		return Point{}, false
-	}
-
-	ta := db.PerpDot(a0.Sub(b0)) / div
-	tb := da.PerpDot(a0.Sub(b0)) / div
-	if 0.0 <= ta && ta <= 1.0 && 0.0 <= tb && tb <= 1.0 {
-		return a0.Interpolate(a1, ta), true
-	}
-	return Point{}, false
-}
-
-// http://mathworld.wolfram.com/Circle-LineIntersection.html
-func intersectionRayCircle(l0, l1, c Point, r float64) (Point, Point, bool) {
-	d := l1.Sub(l0).Norm(1.0) // along line direction, anchored in l0, its length is 1
-	D := l0.Sub(c).PerpDot(d)
-	discriminant := r*r - D*D
-	if discriminant < 0 {
-		return Point{}, Point{}, false
-	}
-	discriminant = math.Sqrt(discriminant)
-
-	ax := D * d.Y
-	bx := d.X * discriminant
-	if d.Y < 0.0 {
-		bx = -bx
-	}
-	ay := -D * d.X
-	by := math.Abs(d.Y) * discriminant
-	return c.Add(Point{ax + bx, ay + by}), c.Add(Point{ax - bx, ay - by}), true
-}
-
-// https://math.stackexchange.com/questions/256100/how-can-i-find-the-points-at-which-two-circles-intersect
-// https://gist.github.com/jupdike/bfe5eb23d1c395d8a0a1a4ddd94882ac
-func intersectionCircleCircle(c0 Point, r0 float64, c1 Point, r1 float64) (Point, Point, bool) {
-	R := c0.Sub(c1).Length()
-	if R < math.Abs(r0-r1) || r0+r1 < R || c0.Equals(c1) {
-		return Point{}, Point{}, false
-	}
-	R2 := R * R
-
-	k := r0*r0 - r1*r1
-	a := 0.5
-	b := 0.5 * k / R2
-	c := 0.5 * math.Sqrt(2.0*(r0*r0+r1*r1)/R2-k*k/(R2*R2)-1.0)
-
-	i0 := c0.Add(c1).Mul(a)
-	i1 := c1.Sub(c0).Mul(b)
-	i2 := Point{c1.Y - c0.Y, c0.X - c1.X}.Mul(c)
-	return i0.Add(i1).Add(i2), i0.Add(i1).Sub(i2), true
-}
-
-////////////////////////////////////////////////////////////////
-
 func ellipsePos(rx, ry, phi, cx, cy, theta float64) Point {
 	sintheta, costheta := math.Sincos(theta)
 	sinphi, cosphi := math.Sincos(phi)
@@ -222,11 +162,10 @@ func arcToCube(start Point, rx, ry, phi float64, large, sweep bool, end Point) *
 
 // see Drawing and elliptical arc using polylines, quadratic or cubic Bézier curves (2003), L. Maisonobe,
 // https://spaceroots.org/documents/ellipse/elliptical-arc.pdf
-// TODO: see S.H. Kim and Y.J. Ahn, An approximation of circular arcs by quartic Bezier curves, Computer-Aided Design 39 (2007, p. 490--493)
 func ellipseToQuadraticBeziers(start Point, rx, ry, phi float64, large, sweep bool, end Point) [][3]Point {
 	cx, cy, theta0, theta1 := ellipseToCenter(start.X, start.Y, rx, ry, phi, large, sweep, end.X, end.Y)
 
-	dtheta := math.Pi / 2.0
+	dtheta := math.Pi / 2.0 // TODO: use error measure to determine dtheta?
 	n := int(math.Ceil(math.Abs(theta1-theta0) / dtheta))
 	dtheta = math.Abs(theta1-theta0) / float64(n) // evenly spread the n points, dalpha will get smaller
 	kappa := math.Tan(dtheta / 2.0)
@@ -252,11 +191,10 @@ func ellipseToQuadraticBeziers(start Point, rx, ry, phi float64, large, sweep bo
 
 // see Drawing and elliptical arc using polylines, quadratic or cubic Bézier curves (2003), L. Maisonobe,
 // https://spaceroots.org/documents/ellipse/elliptical-arc.pdf
-// TODO: see M. Goldapp, Approximation of circular arcs by cubic polynomials, Computer Aided Geometric Design 8 (1991), p. 227--238
 func ellipseToCubicBeziers(start Point, rx, ry, phi float64, large, sweep bool, end Point) [][4]Point {
 	cx, cy, theta0, theta1 := ellipseToCenter(start.X, start.Y, rx, ry, phi, large, sweep, end.X, end.Y)
 
-	dtheta := math.Pi / 2.0
+	dtheta := math.Pi / 2.0 // TODO: use error measure to determine dtheta?
 	n := int(math.Ceil(math.Abs(theta1-theta0) / dtheta))
 	dtheta = math.Abs(theta1-theta0) / float64(n) // evenly spread the n points, dalpha will get smaller
 	kappa := math.Sin(dtheta) * (math.Sqrt(4.0+3.0*math.Pow(math.Tan(dtheta/2.0), 2.0)) - 1.0) / 3.0
@@ -280,6 +218,33 @@ func ellipseToCubicBeziers(start Point, rx, ry, phi float64, large, sweep bool, 
 	}
 	return beziers
 }
+
+// TODO: func ellipseToConicBeziers(start Point, rx, ry, phi float64, large, sweep bool, end Point) ([][3]Point, []float64) {
+//	cx, cy, theta0, theta1 := ellipseToCenter(start.X, start.Y, rx, ry, phi, large, sweep, end.X, end.Y)
+//
+//	dtheta := math.Pi / 2.0
+//	n := int(math.Ceil(math.Abs(theta1-theta0) / dtheta))
+//	dtheta = math.Abs(theta1-theta0) / float64(n) // evenly spread the n points, dalpha will get smaller
+//	if !sweep {
+//		dtheta = -dtheta
+//	}
+//
+//	beziers := [][3]Point{}
+//	weights := []float64{}
+//	startDeriv := ellipseDeriv(rx, ry, phi, sweep, theta0)
+//	for i := 1; i < n+1; i++ {
+//		theta := theta0 + float64(i)*dtheta
+//		end := ellipsePos(rx, ry, phi, cx, cy, theta)
+//		endDeriv := ellipseDeriv(rx, ry, phi, sweep, theta)
+//
+//		beziers = append(beziers, [3]Point{start, cp, end})
+//		weights = append(weights, weight)
+//
+//		startDeriv = endDeriv
+//		start = end
+//	}
+//	return beziers, weights
+//}
 
 func flattenEllipticArc(start Point, rx, ry, phi float64, large, sweep bool, end Point) *Path {
 	// TODO: (flatten ellipse) use direct algorithm
--- path_util_test.go
+++ path_util_test.go
@@ -1,75 +1,11 @@
 package canvas
 
 import (
-	"fmt"
 	"math"
 	"testing"
 
 	"github.com/tdewolff/test"
 )
-
-func TestIntersectionLineLine(t *testing.T) {
-	var tts = []struct {
-		a0, a1 Point
-		b0, b1 Point
-		p      Point
-	}{
-		{Point{2.0, 0.0}, Point{2.0, 3.0}, Point{1.0, 2.0}, Point{3.0, 2.0}, Point{2.0, 2.0}},
-		{Point{2.0, 0.0}, Point{2.0, 1.0}, Point{0.0, 2.0}, Point{1.0, 2.0}, Point{}},
-		{Point{2.0, 0.0}, Point{2.0, 1.0}, Point{3.0, 0.0}, Point{3.0, 1.0}, Point{}},
-	}
-	for i, tt := range tts {
-		t.Run(fmt.Sprint(i), func(t *testing.T) {
-			p, _ := intersectionLineLine(tt.a0, tt.a1, tt.b0, tt.b1)
-			test.T(t, p, tt.p)
-		})
-	}
-}
-
-func TestIntersectionRayCircle(t *testing.T) {
-	var tts = []struct {
-		l0, l1 Point
-		c      Point
-		r      float64
-		p0, p1 Point
-	}{
-		{Point{0.0, 0.0}, Point{0.0, 1.0}, Point{0.0, 0.0}, 2.0, Point{0.0, 2.0}, Point{0.0, -2.0}},
-		{Point{2.0, 0.0}, Point{2.0, 1.0}, Point{0.0, 0.0}, 2.0, Point{2.0, 0.0}, Point{2.0, 0.0}},
-		{Point{0.0, 2.0}, Point{1.0, 2.0}, Point{0.0, 0.0}, 2.0, Point{0.0, 2.0}, Point{0.0, 2.0}},
-		{Point{0.0, 3.0}, Point{1.0, 3.0}, Point{0.0, 0.0}, 2.0, Point{}, Point{}},
-		{Point{0.0, 1.0}, Point{0.0, 0.0}, Point{0.0, 0.0}, 2.0, Point{0.0, 2.0}, Point{0.0, -2.0}},
-	}
-	for i, tt := range tts {
-		t.Run(fmt.Sprint(i), func(t *testing.T) {
-			p0, p1, _ := intersectionRayCircle(tt.l0, tt.l1, tt.c, tt.r)
-			test.T(t, p0, tt.p0)
-			test.T(t, p1, tt.p1)
-		})
-	}
-}
-
-func TestIntersectionCircleCircle(t *testing.T) {
-	var tts = []struct {
-		c0     Point
-		r0     float64
-		c1     Point
-		r1     float64
-		p0, p1 Point
-	}{
-		{Point{0.0, 0.0}, 1.0, Point{2.0, 0.0}, 1.0, Point{1.0, 0.0}, Point{1.0, 0.0}},
-		{Point{0.0, 0.0}, 1.0, Point{1.0, 1.0}, 1.0, Point{1.0, 0.0}, Point{0.0, 1.0}},
-		{Point{0.0, 0.0}, 1.0, Point{3.0, 0.0}, 1.0, Point{}, Point{}},
-		{Point{0.0, 0.0}, 1.0, Point{0.0, 0.0}, 1.0, Point{}, Point{}},
-		{Point{0.0, 0.0}, 1.0, Point{0.5, 0.0}, 2.0, Point{}, Point{}},
-	}
-	for i, tt := range tts {
-		t.Run(fmt.Sprint(i), func(t *testing.T) {
-			p0, p1, _ := intersectionCircleCircle(tt.c0, tt.r0, tt.c1, tt.r1)
-			test.T(t, p0, tt.p0)
-			test.T(t, p1, tt.p1)
-		})
-	}
-}
 
 func TestEllipse(t *testing.T) {
 	test.T(t, ellipsePos(2.0, 1.0, math.Pi/2.0, 1.0, 0.5, 0.0), Point{1.0, 2.5})
