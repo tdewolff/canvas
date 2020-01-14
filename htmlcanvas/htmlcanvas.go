796d338fb819f89633b57f12066502e52a958743
Taco de Wolff <tacodewolff@gmail.com>
2020-01-14 18:02:16 -0300

    Update HTML canvas to keep Style state

--- htmlcanvas/htmlcanvas.go
+++ htmlcanvas/htmlcanvas.go
@@ -9,24 +9,26 @@ import (
 )
 
 type htmlCanvas struct {
-	context       js.Value
+	ctx           js.Value
 	width, height float64
 	dpm           float64
+	style         canvas.Style
 }
 
-func New(canvas js.Value, width, height, dpm float64) *htmlCanvas {
-	canvas.Set("width", width*dpm)
-	canvas.Set("height", height*dpm)
+func New(c js.Value, width, height, dpm float64) *htmlCanvas {
+	c.Set("width", width*dpm)
+	c.Set("height", height*dpm)
 
-	context := canvas.Call("getContext", "2d")
-	context.Call("clearRect", 0, 0, width*dpm, height*dpm)
-	context.Set("imageSmoothingEnabled", true)
-	context.Set("imageSmoothingQuality", "high")
+	ctx := c.Call("getContext", "2d")
+	ctx.Call("clearRect", 0, 0, width*dpm, height*dpm)
+	ctx.Set("imageSmoothingEnabled", true)
+	ctx.Set("imageSmoothingQuality", "high")
 	return &htmlCanvas{
-		context: context,
-		width:   width * dpm,
-		height:  height * dpm,
-		dpm:     dpm,
+		ctx:    ctx,
+		width:  width * dpm,
+		height: height * dpm,
+		dpm:    dpm,
+		style:  canvas.DefaultStyle,
 	}
 }
 
@@ -41,59 +43,85 @@ func (r *htmlCanvas) RenderPath(path *canvas.Path, style canvas.Style, m canvas.
 	path = path.Transform(m)
 	path = path.ReplaceArcs()
 
-	r.context.Call("beginPath")
+	r.ctx.Call("beginPath")
 	path.Iterate(func(start, end canvas.Point) {
-		r.context.Call("moveTo", end.X*r.dpm, r.height-end.Y*r.dpm)
+		r.ctx.Call("moveTo", end.X*r.dpm, r.height-end.Y*r.dpm)
 	}, func(start, end canvas.Point) {
-		r.context.Call("lineTo", end.X*r.dpm, r.height-end.Y*r.dpm)
+		r.ctx.Call("lineTo", end.X*r.dpm, r.height-end.Y*r.dpm)
 	}, func(start, cp, end canvas.Point) {
-		r.context.Call("quadraticCurveTo", cp.X*r.dpm, r.height-cp.Y*r.dpm, end.X*r.dpm, r.height-end.Y*r.dpm)
+		r.ctx.Call("quadraticCurveTo", cp.X*r.dpm, r.height-cp.Y*r.dpm, end.X*r.dpm, r.height-end.Y*r.dpm)
 	}, func(start, cp1, cp2, end canvas.Point) {
-		r.context.Call("bezierCurveTo", cp1.X*r.dpm, r.height-cp1.Y*r.dpm, cp2.X*r.dpm, r.height-cp2.Y*r.dpm, end.X*r.dpm, r.height-end.Y*r.dpm)
+		r.ctx.Call("bezierCurveTo", cp1.X*r.dpm, r.height-cp1.Y*r.dpm, cp2.X*r.dpm, r.height-cp2.Y*r.dpm, end.X*r.dpm, r.height-end.Y*r.dpm)
 	}, func(start canvas.Point, rx, ry, rot float64, large, sweep bool, end canvas.Point) {
 		panic("arcs should have been replaced")
 	}, func(start, end canvas.Point) {
-		r.context.Call("closePath")
+		r.ctx.Call("closePath")
 	})
 
 	if style.FillColor.A != 0 {
-		r.context.Set("fillStyle", canvas.CSSColor(style.FillColor).String())
-		r.context.Call("fill")
+		if style.FillColor != r.style.FillColor {
+			r.ctx.Set("fillStyle", canvas.CSSColor(style.FillColor).String())
+		}
+		r.ctx.Call("fill")
 	}
 	if style.StrokeColor.A != 0 && 0.0 < style.StrokeWidth {
-		if _, ok := style.StrokeCapper.(canvas.RoundCapper); ok {
-			r.context.Set("lineCap", "round")
-		} else if _, ok := style.StrokeCapper.(canvas.SquareCapper); ok {
-			r.context.Set("lineCap", "square")
-		} else if _, ok := style.StrokeCapper.(canvas.ButtCapper); ok {
-			r.context.Set("lineCap", "butt")
-		} else {
-			panic("HTML Canvas: line cap not support")
+		if style.StrokeCapper != r.style.StrokeCapper {
+			if _, ok := style.StrokeCapper.(canvas.RoundCapper); ok {
+				r.ctx.Set("lineCap", "round")
+			} else if _, ok := style.StrokeCapper.(canvas.SquareCapper); ok {
+				r.ctx.Set("lineCap", "square")
+			} else if _, ok := style.StrokeCapper.(canvas.ButtCapper); ok {
+				r.ctx.Set("lineCap", "butt")
+			} else {
+				panic("HTML Canvas: line cap not support")
+			}
 		}
 
-		if _, ok := style.StrokeJoiner.(canvas.BevelJoiner); ok {
-			r.context.Set("lineJoin", "bevel")
-		} else if _, ok := style.StrokeJoiner.(canvas.RoundJoiner); ok {
-			r.context.Set("lineJoin", "round")
-		} else if miter, ok := style.StrokeJoiner.(canvas.MiterJoiner); ok && !math.IsNaN(miter.Limit) && miter.GapJoiner == canvas.BevelJoin {
-			r.context.Set("lineJoin", "miter")
-			r.context.Set("miterLimit", miter.Limit)
-		} else {
-			panic("HTML Canvas: line join not support")
+		if style.StrokeJoiner != r.style.StrokeJoiner {
+			if _, ok := style.StrokeJoiner.(canvas.BevelJoiner); ok {
+				r.ctx.Set("lineJoin", "bevel")
+			} else if _, ok := style.StrokeJoiner.(canvas.RoundJoiner); ok {
+				r.ctx.Set("lineJoin", "round")
+			} else if miter, ok := style.StrokeJoiner.(canvas.MiterJoiner); ok && !math.IsNaN(miter.Limit) && miter.GapJoiner == canvas.BevelJoin {
+				r.ctx.Set("lineJoin", "miter")
+				r.ctx.Set("miterLimit", miter.Limit)
+			} else {
+				panic("HTML Canvas: line join not support")
+			}
 		}
 
-		dashes := []interface{}{}
-		for _, dash := range style.Dashes {
-			dashes = append(dashes, dash*r.dpm)
+		dashesEqual := len(style.Dashes) == len(r.style.Dashes)
+		if dashesEqual {
+			for i, dash := range style.Dashes {
+				if dash != r.style.Dashes[i] {
+					dashesEqual = false
+					break
+				}
+			}
 		}
-		jsDashes := js.Global().Get("Array").New(dashes...)
-		r.context.Call("setLineDash", jsDashes)
-		r.context.Set("lineDashOffset", style.DashOffset*r.dpm)
 
-		r.context.Set("lineWidth", style.StrokeWidth*r.dpm)
-		r.context.Set("strokeStyle", canvas.CSSColor(style.StrokeColor).String())
-		r.context.Call("stroke")
+		if !dashesEqual {
+			dashes := []interface{}{}
+			for _, dash := range style.Dashes {
+				dashes = append(dashes, dash*r.dpm)
+			}
+			jsDashes := js.Global().Get("Array").New(dashes...)
+			r.ctx.Call("setLineDash", jsDashes)
+		}
+
+		if style.DashOffset != r.style.DashOffset {
+			r.ctx.Set("lineDashOffset", style.DashOffset*r.dpm)
+		}
+
+		if style.StrokeWidth != r.style.StrokeWidth {
+			r.ctx.Set("lineWidth", style.StrokeWidth*r.dpm)
+		}
+		if style.StrokeColor != r.style.StrokeColor {
+			r.ctx.Set("strokeStyle", canvas.CSSColor(style.StrokeColor).String())
+		}
+		r.ctx.Call("stroke")
 	}
+	r.style = style
 }
 
 func (r *htmlCanvas) RenderText(text *canvas.Text, m canvas.Matrix) {
@@ -160,7 +188,7 @@ func (r *htmlCanvas) RenderImage(img image.Image, m canvas.Matrix) {
 
 	origin := m.Dot(canvas.Point{0, float64(img.Bounds().Size().Y)}).Mul(r.dpm)
 	m = m.Scale(r.dpm, r.dpm)
-	r.context.Call("setTransform", m[0][0], m[0][1], m[1][0], m[1][1], origin.X, r.height-origin.Y)
-	r.context.Call("drawImage", imageBitmap, 0, 0)
-	r.context.Call("setTransform", 1.0, 0.0, 0.0, 1.0, 0.0, 0.0)
+	r.ctx.Call("setTransform", m[0][0], m[0][1], m[1][0], m[1][1], origin.X, r.height-origin.Y)
+	r.ctx.Call("drawImage", imageBitmap, 0, 0)
+	r.ctx.Call("setTransform", 1.0, 0.0, 0.0, 1.0, 0.0, 0.0)
 }
