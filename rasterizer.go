b9781e7def7cede5af039e568f872f121adc28d7
Taco de Wolff <tacodewolff@gmail.com>
2020-04-01 17:49:37 -0300

    Fix image placement for rasterizer, fixes #32

--- rasterizer.go
+++ rasterizer.go
@@ -92,17 +92,18 @@ func (r *Rasterizer) RenderText(text *Text, m Matrix) {
 }
 
 func (r *Rasterizer) RenderImage(img image.Image, m Matrix) {
-	origin := m.Dot(Point{0, float64(img.Bounds().Size().Y)}).Mul(r.dpm)
-	m = m.Scale(r.dpm, r.dpm)
-
-	h := float64(r.img.Bounds().Size().Y)
-	aff3 := f64.Aff3{m[0][0], -m[0][1], origin.X, -m[1][0], m[1][1], h - origin.Y}
-
 	// add transparent margin to image for smooth borders when rotating
 	margin := 4
 	size := img.Bounds().Size()
 	img2 := image.NewRGBA(image.Rect(0, 0, size.X+margin*2, size.Y+margin*2))
 	draw.Draw(img2, image.Rect(margin, margin, size.X, size.Y), img, image.Point{}, draw.Over)
 
+	// draw to destination image
+	// note that we need to correct for the added margin in origin and m
+	origin := m.Dot(Point{-float64(margin), float64(img2.Bounds().Size().Y - margin)}).Mul(r.dpm)
+	m = m.Scale(r.dpm*(float64(size.X+margin)/float64(size.X)), r.dpm*(float64(size.Y+margin)/float64(size.Y)))
+
+	h := float64(r.img.Bounds().Size().Y)
+	aff3 := f64.Aff3{m[0][0], -m[0][1], origin.X, -m[1][0], m[1][1], h - origin.Y}
 	draw.CatmullRom.Transform(r.img, aff3, img2, img2.Bounds(), draw.Over, nil)
 }
