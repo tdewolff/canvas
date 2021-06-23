fcfdd6fd1fbbb137e776bd08afaa779535024390
Taco de Wolff <tacodewolff@gmail.com>
2021-06-23 17:33:44 -0400

    Fix gamma correction for Gio again, Gio does gamma-correct blending as opposed to most renderers

--- renderers/gio/gio.go
+++ renderers/gio/gio.go
@@ -3,7 +3,6 @@ package gio
 import (
 	"image"
 	"image/color"
-	"math"
 
 	"gioui.org/f32"
 	"gioui.org/layout"
@@ -108,7 +107,7 @@ func (r *Gio) renderPath(path *canvas.Path, col color.RGBA) {
 	}
 
 	shape := clip.Outline{p.End()}
-	paint.FillShape(r.ops, RGBAToNRGBA(col), shape.Op())
+	paint.FillShape(r.ops, toNRGBA(col), shape.Op())
 }
 
 // RenderPath renders a path to the canvas using a style and a transformation matrix.
@@ -145,62 +144,13 @@ func (r *Gio) RenderImage(img image.Image, m canvas.Matrix) {
 	paint.PaintOp{}.Add(r.ops)
 }
 
-// from https://git.sr.ht/~eliasnaur/gio/tree/main/item/internal/f32color/rgba.go#L97
-func RGBAToNRGBA(col color.RGBA) color.NRGBA {
-	if col.A == 0xFF {
-		return color.NRGBA(col)
-	} else if col.A == 0 {
+func toNRGBA(col color.Color) color.NRGBA {
+	r, g, b, a := col.RGBA()
+	if a == 0 {
 		return color.NRGBA{}
 	}
-
-	linear := RGBA{
-		R: sRGBToLinear(float32(col.R) / 0xff),
-		G: sRGBToLinear(float32(col.G) / 0xff),
-		B: sRGBToLinear(float32(col.B) / 0xff),
-		A: float32(col.A) / 0xff,
-	}
-	return linear.SRGB()
-}
-
-// RGBA is a 32 bit floating point linear premultiplied color space.
-type RGBA struct {
-	R, G, B, A float32
-}
-
-// SRGBA converts from linear to sRGB color space.
-func (col RGBA) SRGB() color.NRGBA {
-	if col.A == 0 {
-		return color.NRGBA{}
-	}
-	return color.NRGBA{
-		R: uint8(linearTosRGB(col.R/col.A)*255 + .5),
-		G: uint8(linearTosRGB(col.G/col.A)*255 + .5),
-		B: uint8(linearTosRGB(col.B/col.A)*255 + .5),
-		A: uint8(col.A*255 + .5),
-	}
-}
-
-// linearTosRGB transforms color value from linear to sRGB.
-func linearTosRGB(c float32) float32 {
-	// Formula from EXT_sRGB.
-	switch {
-	case c <= 0:
-		return 0
-	case 0 < c && c < 0.0031308:
-		return 12.92 * c
-	case 0.0031308 <= c && c < 1:
-		return 1.055*float32(math.Pow(float64(c), 0.41666)) - 0.055
-	}
-
-	return 1
-}
-
-// sRGBToLinear transforms color value from sRGB to linear.
-func sRGBToLinear(c float32) float32 {
-	// Formula from EXT_sRGB.
-	if c <= 0.04045 {
-		return c / 12.92
-	} else {
-		return float32(math.Pow(float64((c+0.055)/1.055), 2.4))
-	}
+	r = (r * 0xffff) / a
+	g = (g * 0xffff) / a
+	b = (b * 0xffff) / a
+	return color.NRGBA{R: uint8(r >> 8), G: uint8(g >> 8), B: uint8(b >> 8), A: uint8(a >> 8)}
 }
