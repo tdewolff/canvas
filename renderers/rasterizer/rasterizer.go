ef2a2868e50e9600fa5ce0d645dccf08a55d6549
Taco de Wolff <tacodewolff@gmail.com>
2021-06-25 11:32:02 -0400

    Add support for gamma-corrected rasterization, New is renamed to FromImage as New will now implicitly create a new image

--- renderers/rasterizer/rasterizer.go
+++ renderers/rasterizer/rasterizer.go
@@ -7,6 +7,7 @@ import (
 	"image/jpeg"
 	"image/png"
 	"io"
+	"log"
 
 	"github.com/tdewolff/canvas"
 	"golang.org/x/image/bmp"
@@ -17,54 +18,61 @@ import (
 )
 
 // PNGWriter writes the canvas as a PNG file.
+// DEPRECATED
 func PNGWriter(resolution canvas.Resolution) canvas.Writer {
+	log.Println("WARNING: github.com/tdewolff/canvas/renderers/rasterizer.PNGWriter is deprecated, please use github.com/tdewolff/canvas/renderers.PNG")
 	return func(w io.Writer, c *canvas.Canvas) error {
-		img := Draw(c, resolution)
+		img := Draw(c, resolution, nil)
 		return png.Encode(w, img)
 	}
 }
 
 // JPGWriter writes the canvas as a JPG file.
+// DEPRECATED
 func JPGWriter(resolution canvas.Resolution, opts *jpeg.Options) canvas.Writer {
+	log.Println("WARNING: github.com/tdewolff/canvas/renderers/rasterizer.JPGWriter is deprecated, please use github.com/tdewolff/canvas/renderers.JPG")
 	return func(w io.Writer, c *canvas.Canvas) error {
-		img := Draw(c, resolution)
+		img := Draw(c, resolution, nil)
 		return jpeg.Encode(w, img, opts)
 	}
 }
 
 // GIFWriter writes the canvas as a GIF file.
+// DEPRECATED
 func GIFWriter(resolution canvas.Resolution, opts *gif.Options) canvas.Writer {
+	log.Println("WARNING: github.com/tdewolff/canvas/renderers/rasterizer.GIFWriter is deprecated, please use github.com/tdewolff/canvas/renderers.GIF")
 	return func(w io.Writer, c *canvas.Canvas) error {
-		img := Draw(c, resolution)
+		img := Draw(c, resolution, nil)
 		return gif.Encode(w, img, opts)
 	}
 }
 
 // TIFFWriter writes the canvas as a TIFF file.
+// DEPRECATED
 func TIFFWriter(resolution canvas.Resolution, opts *tiff.Options) canvas.Writer {
+	log.Println("WARNING: github.com/tdewolff/canvas/renderers/rasterizer.TIFFWriter is deprecated, please use github.com/tdewolff/canvas/renderers.TIFF")
 	return func(w io.Writer, c *canvas.Canvas) error {
-		img := Draw(c, resolution)
+		img := Draw(c, resolution, nil)
 		return tiff.Encode(w, img, opts)
 	}
 }
 
 // BMPWriter writes the canvas as a BMP file.
+// DEPRECATED
 func BMPWriter(resolution canvas.Resolution) canvas.Writer {
+	log.Println("WARNING: github.com/tdewolff/canvas/renderers/rasterizer.BMPWriter is deprecated, please use github.com/tdewolff/canvas/renderers.BMP")
 	return func(w io.Writer, c *canvas.Canvas) error {
-		img := Draw(c, resolution)
+		img := Draw(c, resolution, nil)
 		return bmp.Encode(w, img)
 	}
 }
 
 // Draw draws the canvas on a new image with given resolution (in dots-per-millimeter). Higher resolution will result in larger images.
-func Draw(c *canvas.Canvas, resolution canvas.Resolution) *image.RGBA {
+func Draw(c *canvas.Canvas, resolution canvas.Resolution, colorSpace canvas.ColorSpace) *image.RGBA {
 	img := image.NewRGBA(image.Rect(0, 0, int(c.W*resolution.DPMM()+0.5), int(c.H*resolution.DPMM()+0.5)))
-	ras := New(img, resolution)
+	ras := FromImage(img, resolution, colorSpace)
 	c.Render(ras)
-	if _, ok := ras.ColorSpace.(canvas.LinearColorSpace); !ok {
-		// gamma compress
-		changeColorSpace(img, ras.Image, ras.ColorSpace.FromLinear)
-	}
+	ras.Close()
 	return img
 }
 
@@ -72,33 +80,32 @@ func Draw(c *canvas.Canvas, resolution canvas.Resolution) *image.RGBA {
 type Rasterizer struct {
 	draw.Image
 	resolution canvas.Resolution
-
 	ColorSpace canvas.ColorSpace
 }
 
-// New returns a renderer that draws to a rasterized image. By default the linear color space is used, which assumes input and output colors are in linearRGB. If the sRGB color space is used for drawing with an average of gamma=2.2, the input and output colors are assumed to be in sRGB and blending happens in linearRGB.aBe aware that for text this results in thin stems for black-on-white (but wide stems for white-on-black).
-func New(img draw.Image, resolution canvas.Resolution) *Rasterizer {
+// New returns a renderer that draws to a rasterized image. By default the linear color space is used, which assumes input and output colors are in linearRGB. If the sRGB color space is used for drawing with an average of gamma=2.2, the input and output colors are assumed to be in sRGB and blending happens in linearRGB. Be aware that for text this results in thin stems for black-on-white (but wide stems for white-on-black).
+func New(width, height float64, resolution canvas.Resolution, colorSpace canvas.ColorSpace) *Rasterizer {
+	img := image.NewRGBA(image.Rect(0, 0, int(width*resolution.DPMM()+0.5), int(height*resolution.DPMM()+0.5)))
+	return FromImage(img, resolution, colorSpace)
+}
+
+// FromImage returns a renderer that draws to an existing image.
+func FromImage(img draw.Image, resolution canvas.Resolution, colorSpace canvas.ColorSpace) *Rasterizer {
+	if colorSpace == nil {
+		colorSpace = canvas.DefaultColorSpace
+	}
 	return &Rasterizer{
 		Image:      img,
 		resolution: resolution,
-		ColorSpace: canvas.SRGBColorSpace{},
+		ColorSpace: colorSpace,
 	}
 }
 
-// NewImage returns a renderer that draws to a new rasterized image.
-func NewImage(width, height float64, resolution canvas.Resolution) *Rasterizer {
-	img := image.NewRGBA(image.Rect(0, 0, int(width*resolution.DPMM()+0.5), int(height*resolution.DPMM()+0.5)))
-	return New(img, resolution)
-}
-
-// Image returns the image in the sRGB color space. It copies the underlying image buffer if the used color model by the rasterizer is not LinearColorModel (default).
-func (r *Rasterizer) RGBA() *image.RGBA {
-	img := image.NewRGBA(r.Bounds())
+func (r *Rasterizer) Close() {
 	if _, ok := r.ColorSpace.(canvas.LinearColorSpace); !ok {
 		// gamma compress
-		changeColorSpace(img, r.Image, r.ColorSpace.FromLinear)
+		changeColorSpace(r.Image, r.Image, r.ColorSpace.FromLinear)
 	}
-	return img
 }
 
 // Size returns the size of the canvas in millimeters.
@@ -206,11 +213,20 @@ func (r *Rasterizer) RenderImage(img image.Image, m canvas.Matrix) {
 
 type colorFunc func(color.Color) color.RGBA
 
-func changeColorSpace(dst *image.RGBA, src image.Image, f colorFunc) {
-	for j := 0; j < dst.Bounds().Max.Y; j++ {
-		for i := 0; i < dst.Bounds().Max.X; i++ {
-			// TODO: parallelize
-			dst.SetRGBA(i, j, f(src.At(i, j)))
+func changeColorSpace(dst draw.Image, src image.Image, f colorFunc) {
+	if dstRGBA, ok := dst.(*image.RGBA); ok {
+		for j := 0; j < dst.Bounds().Max.Y; j++ {
+			for i := 0; i < dst.Bounds().Max.X; i++ {
+				// TODO: parallelize
+				dstRGBA.SetRGBA(i, j, f(src.At(i, j)))
+			}
+		}
+	} else {
+		for j := 0; j < dst.Bounds().Max.Y; j++ {
+			for i := 0; i < dst.Bounds().Max.X; i++ {
+				// TODO: parallelize
+				dst.Set(i, j, f(src.At(i, j)))
+			}
 		}
 	}
 }
