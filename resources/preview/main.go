31c160f7a1d3dbcbc86791aaae6b46cd865d4218
Taco de Wolff <tacodewolff@gmail.com>
2022-12-01 15:59:15 -0300

    Add smiley to preview

--- resources/preview/main.go
+++ resources/preview/main.go
@@ -77,7 +77,22 @@ func draw(c *canvas.Context) {
 	rt.Add(fontLatin.Face(pt, canvas.Black, canvas.FontRegular, canvas.FontNormal, canvas.FontUnderline), "sodales")
 	rt.Add(face, " eros vel ")
 	rt.Add(fontLatin.Face(pt, canvas.Black, canvas.FontRegular, canvas.FontNormal, canvas.FontSineUnderline), "nibh")
-	rt.Add(face, " fringilla pellen\u200btesque eu cillum. ")
+	rt.Add(face, " fringilla pellen\u200btesque eu ")
+
+	c2 := canvas.New(6.144, 6.144)
+	ctx2 := canvas.NewContext(c2)
+	ctx2.SetView(canvas.Identity.Translate(0.0, 6.144).Scale(0.05, -0.05))
+	// face
+	ctx2.SetFillColor(canvas.Hex("#fbd433"))
+	ctx2.DrawPath(0.0, 0.0, canvas.MustParseSVG("M45.54,2.11A61.42,61.42,0,1,1,2.11,77.34,61.42,61.42,0,0,1,45.54,2.11Z"))
+	// eyes
+	ctx2.SetFillColor(canvas.Hex("#141518"))
+	ctx2.DrawPath(0.0, 0.0, canvas.MustParseSVG("M45.78,32.27c4.3,0,7.79,5,7.79,11.27s-3.49,11.27-7.79,11.27S38,49.77,38,43.54s3.48-11.27,7.78-11.27Z"))
+	ctx2.DrawPath(0.0, 0.0, canvas.MustParseSVG("M77.1,32.27c4.3,0,7.78,5,7.78,11.27S81.4,54.81,77.1,54.81s-7.79-5-7.79-11.27S72.8,32.27,77.1,32.27Z"))
+	// mouth
+	ctx2.DrawPath(0.0, 0.0, canvas.MustParseSVG("M28.8,70.82a39.65,39.65,0,0,0,8.83,8.41,42.72,42.72,0,0,0,25,7.53,40.44,40.44,0,0,0,24.12-8.12,35.75,35.75,0,0,0,7.49-7.87.22.22,0,0,1,.31,0L97,73.14a.21.21,0,0,1,0,.29A45.87,45.87,0,0,1,82.89,88.58,37.67,37.67,0,0,1,62.83,95a39,39,0,0,1-20.68-5.55A50.52,50.52,0,0,1,25.9,73.57a.23.23,0,0,1,0-.28l2.52-2.5a.22.22,0,0,1,.32,0l0,0Z"))
+	rt.AddCanvas(c2, canvas.FontMiddle)
+	rt.Add(face, " cillum. ")
 
 	face = fontLatin.Face(pt, canvas.Black, canvas.FontRegular, canvas.FontNormal)
 	face.Language = "ru"
@@ -103,7 +118,7 @@ func draw(c *canvas.Context) {
 	c.DrawPath(100, 5, p.Stroke(0.75, canvas.RoundCap, canvas.RoundJoin))
 
 	// Draw a LaTeX formula
-	latex, err := canvas.ParseLaTeX(`$y = \sin\left(\frac{x}{180}\pi\right)$`)
+	latex, err := canvas.ParseLaTeX(`$y = \sin\left(\frac{x}{180}\pi\right)$`, 12.0)
 	if err != nil {
 		panic(err)
 	}
