14cf1e9a6ed7618193ab9be2cc3acf36f97529ba
Taco de Wolff <tacodewolff@gmail.com>
2022-12-21 21:27:08 -0300

    Fix OpenGL example, fixes #194

--- examples/opengl/main.go
+++ examples/opengl/main.go
@@ -28,8 +28,8 @@ func main() {
 	defer glfw.Terminate()
 
 	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 3)
-	glfw.WindowHint(glfw.ContextVersionMinor, 2)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
