111570319a74516919e8bf2db6ac2b115fe96bbe
Taco de Wolff <tacodewolff@gmail.com>
2023-04-15 19:18:09 -0400

    Move gradient code to colors.go

--- colors.go
+++ colors.go
@@ -54,6 +54,241 @@ func Hex(s string) color.RGBA {
 	return Black
 }
 
+// Gradient is a gradient pattern for filling.
+type Gradient interface {
+	SetColorSpace(ColorSpace) Gradient
+	At(float64, float64) color.RGBA
+}
+
+// Stop is a color and offset for gradient patterns.
+type Stop struct {
+	Offset float64
+	Color  color.RGBA
+}
+
+// Stops are the colors and offsets for gradient patterns, sorted by offset.
+type Stops []Stop
+
+// Add adds a new color stop to a gradient.
+func (stops *Stops) Add(t float64, color color.RGBA) {
+	stop := Stop{math.Min(math.Max(t, 0.0), 1.0), color}
+	// insert or replace stop and keep sort order
+	for i := range *stops {
+		if Equal((*stops)[i].Offset, stop.Offset) {
+			(*stops)[i] = stop
+			return
+		} else if stop.Offset < (*stops)[i].Offset {
+			*stops = append((*stops)[:i], append(Stops{stop}, (*stops)[i:]...)...)
+			return
+		}
+	}
+	*stops = append(*stops, stop)
+}
+
+// At returns the color at position t ∈ [0,1].
+func (stops Stops) At(t float64) color.RGBA {
+	if len(stops) == 0 {
+		return Transparent
+	} else if t <= 0.0 || len(stops) == 1 {
+		return stops[0].Color
+	} else if 1.0 <= t {
+		return stops[len(stops)-1].Color
+	}
+	for i, stop := range stops[1:] {
+		if t < stop.Offset {
+			t = (t - stops[i].Offset) / (stop.Offset - stops[i].Offset)
+			return colorLerp(stops[i].Color, stop.Color, t)
+		}
+	}
+	return stops[len(stops)-1].Color
+}
+
+func colorLerp(c0, c1 color.RGBA, t float64) color.RGBA {
+	r0, g0, b0, a0 := c0.RGBA()
+	r1, g1, b1, a1 := c1.RGBA()
+	return color.RGBA{
+		lerp(r0, r1, t),
+		lerp(g0, g1, t),
+		lerp(b0, b1, t),
+		lerp(a0, a1, t),
+	}
+}
+
+func lerp(a, b uint32, t float64) uint8 {
+	return uint8(uint32((1.0-t)*float64(a)+t*float64(b)) >> 8)
+}
+
+// LinearGradient is a linear gradient pattern between the given start and end points. The color at offset 0 corresponds to the start position, and offset 1 to the end position. Start and end points are in the canvas's coordinate system.
+type LinearGradient struct {
+	Start, End Point
+	Stops
+
+	d  Point
+	d2 float64
+}
+
+// NewLinearGradient returns a new linear gradient pattern.
+func NewLinearGradient(start, end Point) *LinearGradient {
+	d := end.Sub(start)
+	return &LinearGradient{
+		Start: start,
+		End:   end,
+
+		d:  d,
+		d2: d.Dot(d),
+	}
+}
+
+// SetColorSpace returns the linear gradient with the given color space. Automatically called by the rasterizer.
+func (g *LinearGradient) SetColorSpace(colorSpace ColorSpace) Gradient {
+	if _, ok := colorSpace.(LinearColorSpace); ok {
+		return g
+	}
+	gradient := &(*g)
+	for i := range gradient.Stops {
+		gradient.Stops[i].Color = colorSpace.ToLinear(gradient.Stops[i].Color)
+	}
+	return gradient
+}
+
+// At returns the color at position (x,y).
+func (g *LinearGradient) At(x, y float64) color.RGBA {
+	if len(g.Stops) == 0 {
+		return Transparent
+	}
+
+	p := Point{x, y}.Sub(g.Start)
+	if Equal(g.d.Y, 0.0) && !Equal(g.d.X, 0.0) {
+		return g.Stops.At(p.X / g.d.X) // horizontal
+	} else if !Equal(g.d.Y, 0.0) && Equal(g.d.X, 0.0) {
+		return g.Stops.At(p.Y / g.d.Y) // vertical
+	}
+	t := p.Dot(g.d) / g.d2
+	return g.Stops.At(t)
+}
+
+// RadialGradient is a radial gradient pattern between two circles defined by their center points and radii. Color stop at offset 0 corresponds to the first circle and offset 1 to the second circle.
+type RadialGradient struct {
+	C0, C1 Point
+	R0, R1 float64
+	Stops
+
+	cd    Point
+	dr, a float64
+}
+
+// NewRadialGradient returns a new radial gradient pattern.
+func NewRadialGradient(c0 Point, r0 float64, c1 Point, r1 float64) *RadialGradient {
+	cd := c1.Sub(c0)
+	dr := r1 - r0
+	return &RadialGradient{
+		C0: c0,
+		R0: r0,
+		C1: c1,
+		R1: r1,
+
+		cd: cd,
+		dr: dr,
+		a:  cd.Dot(cd) - dr*dr,
+	}
+}
+
+// SetColorSpace returns the linear gradient with the given color space. Automatically called by the rasterizer.
+func (g *RadialGradient) SetColorSpace(colorSpace ColorSpace) Gradient {
+	if _, ok := colorSpace.(LinearColorSpace); ok {
+		return g
+	}
+	gradient := *g
+	for i := range gradient.Stops {
+		gradient.Stops[i].Color = colorSpace.ToLinear(gradient.Stops[i].Color)
+	}
+	return &gradient
+}
+
+// At returns the color at position (x,y).
+func (g *RadialGradient) At(x, y float64) color.RGBA {
+	if len(g.Stops) == 0 {
+		return Transparent
+	}
+
+	// see reference implementation of pixman-radial-gradient
+	// https://github.com/servo/pixman/blob/master/pixman/pixman-radial-gradient.c#L161
+	pd := Point{x, y}.Sub(g.C0)
+	b := pd.Dot(g.cd) + g.R0*g.dr
+	c := pd.Dot(pd) - g.R0*g.R0
+	t0, t1 := solveQuadraticFormula(g.a, -2.0*b, c)
+	if !math.IsNaN(t1) {
+		return g.Stops.At(t1)
+	} else if !math.IsNaN(t0) {
+		return g.Stops.At(t0)
+	}
+	return Transparent
+}
+
+// ImagePattern is an image tiling pattern of an image drawn from an origin with a certain resolution. Higher resolution will give smaller tilings.
+//type ImagePattern struct {
+//	img    *image.RGBA
+//	res    Resolution
+//	origin Point
+//}
+//
+//// NewImagePattern returns a new image pattern.
+//func NewImagePattern(iimg image.Image, res Resolution, origin Point) *ImagePattern {
+//	img, ok := iimg.(*image.RGBA)
+//	if !ok {
+//		bounds := iimg.Bounds()
+//		img = image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+//		draw.Draw(img, img.Bounds(), iimg, bounds.Min, draw.Src)
+//	}
+//	return &ImagePattern{
+//		img:    img,
+//		res:    res,
+//		origin: origin,
+//	}
+//}
+//
+//// SetColorSpace returns the linear gradient with the given color space. Automatically called by the rasterizer.
+//func (p *ImagePattern) SetColorSpace(colorSpace ColorSpace) Pattern {
+//	if _, ok := colorSpace.(LinearColorSpace); ok {
+//		return p
+//	}
+//	// TODO: optimize
+//	pattern := *p
+//	bounds := p.img.Bounds()
+//	pattern.img = image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+//	draw.Draw(pattern.img, pattern.img.Bounds(), p.img, bounds.Min, draw.Src)
+//	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+//		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+//			col := pattern.img.RGBAAt(x, y)
+//			col = colorSpace.ToLinear(col)
+//			pattern.img.SetRGBA(x, y, col)
+//		}
+//	}
+//	return &pattern
+//}
+//
+//// At returns the color at position (x,y).
+//func (p *ImagePattern) At(x, y float64) color.RGBA {
+//	x = (x - p.origin.X) * p.res.DPMM()
+//	y = (y - p.origin.Y) * p.res.DPMM()
+//
+//	var s [4]uint8
+//	ix0, iy0 := int(x), int(y)
+//	fx, fy := x-float64(ix0), y-float64(iy0)
+//	ix0 = ix0 % p.img.Bounds().Dx()
+//	iy0 = iy0 % p.img.Bounds().Dy()
+//	ix1 := (ix0 + 1) % p.img.Bounds().Dx()
+//	iy1 := (iy0 + 1) % p.img.Bounds().Dy()
+//	d00 := p.img.PixOffset(ix0, iy0)
+//	d10 := p.img.PixOffset(ix1, iy0)
+//	d01 := p.img.PixOffset(ix0, iy1)
+//	d11 := p.img.PixOffset(ix1, iy1)
+//	for i := 0; i < 4; i++ {
+//		s[i] = uint8((1.0-fy)*((1.0-fx)*float64(p.img.Pix[d00+i])+fx*float64(p.img.Pix[d10+i])) + fy*((1.0-fx)*float64(p.img.Pix[d01+i])+fx*float64(p.img.Pix[d11+i])) + 0.5)
+//	}
+//	return color.RGBA{s[0], s[1], s[2], s[3]}
+//}
+
 // ColorSpace defines the color space within the RGB color model. All colors passed to this library are assumed to be in the sRGB color space, which is a ubiquitous assumption in most software. This works great for most applications, but fails when blending semi-transparent layers. See an elaborate explanation at https://blog.johnnovak.net/2016/09/21/what-every-coder-should-know-about-gamma/, which goes into depth of the problems of using sRGB for blending and the need for gamma correction. In short, we need to transform the colors, which are in the sRGB color space, to the linear color space, perform blending, and then transform them back to the sRGB color space.
 // Unfortunately, almost all software does blending the wrong way (all PDF renderers and browsers I've tested), so by default this library will do the same by using LinearColorSpace which does no conversion from sRGB to linear and back but blends directly in sRGB. Or in other words, it assumes that colors are given in the linear color space and that the output image is expected to be in the linear color space as well. For technical correctness we should really be using the SRGBColorSpace, which will convert from sRGB to linear space, do blending in linear space, and then go back to sRGB space.
 type ColorSpace interface {
