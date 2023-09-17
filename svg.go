9a497c30960d32c518f0bbaa8149ddaf39ad4815
Taco de Wolff <tacodewolff@gmail.com>
2023-09-17 09:31:42 -0300

    Parse SVG <defs> void token, see #251

--- svg.go
+++ svg.go
@@ -900,7 +900,9 @@ func ParseSVG(r io.Reader) (*Canvas, error) {
 				}
 				break
 			} else if tag == "defs" {
-				svg.parseDefs(l)
+				if tt != xml.StartTagCloseVoidToken {
+					svg.parseDefs(l)
+				}
 				break
 			}
 
