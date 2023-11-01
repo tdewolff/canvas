a9af92be27e87bc350f82f71e8f038a924bd2e30
Taco de Wolff <tacodewolff@gmail.com>
2023-10-31 22:28:22 -0300

    Fix pdftext encoding bug

--- cmd/pdftext/encoding.go
+++ cmd/pdftext/encoding.go
@@ -474,7 +474,9 @@ func (r *pdfReader) GetFont(dict pdfDict, name pdfName) (pdfFont, error) {
 								return nil, fmt.Errorf("character name doesn't exist: %v", name)
 							}
 							f.diffs[byte(code)] = r
-							f.diffsRev[r] = byte(code)
+							if _, ok := f.diffsRev[r]; !ok {
+								f.diffsRev[r] = byte(code)
+							}
 							code++
 							i++
 						}
